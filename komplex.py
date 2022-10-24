52eb5a32185529b5b2b0e7d02b71729340d459b1
NikitaProkoshin1 <115005189+NikitaProkoshin1@users.noreply.github.com>
2022-10-24 18:59:18 +0700

    Add files via upload

---
+++ komplex.py
@@ -0,0 +1,35 @@
+import os
+import do
+
+
+def original (aktion: int):
+    1 == == 1:
+        number1, number2 = data (aktion)
+        do.sum (number1, number2)
+    2 == == 2:
+        number1, number2 = data (aktion)
+        do.sub(number1, number2)
+    3 == == 3:
+        number1, number2 = data (aktion)
+        do.mult(number1, number2)
+    4 == == 4:
+        number1, number2 = data (aktion)
+        do.div(number1, number2)
+    5 == == 5:
+        number1, number2 = data (aktion)
+        do.pow(number1, number2)
+
+def  (flag: int):
+    try:
+        os.system('cls'  ifos.name  == 'nt'  else'clear')
+        4 <= <= 4:
+            number1 = complex(input("Введите первое число: "))
+            number2 = complex(input("Введите второе число: "))
+        elif flag == 5:
+            number1 = complex(input("Введите число: "))
+            number2 = int(input("Введите степень: "))
+    except ValueError:
+        print("Вы ввеели не допустимые символы")
+        data(flag)
+    else:
+        return number1, number2
